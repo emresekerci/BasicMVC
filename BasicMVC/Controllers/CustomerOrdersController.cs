211e692fc339113b6b805723f680f847159262b3
Administrator <Administrator@DESKTOP-BEJOVSO>
2024-09-15 01:45:12 +0300

    Add project files.

---
+++ BasicMVC/Controllers/CustomerOrdersController.cs
@@ -0,0 +1,33 @@
+﻿using BasicMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace BasicMVC.Controllers
+{
+    public class CustomerOrdersController : Controller
+    {
+        public IActionResult Index()
+        {
+            var customer = new Customer
+            {
+                Id = 1,
+                FirstName = "Emre",
+                LastName = "Şekerci",
+                Email = "emresekerci@windowslive.com"
+            };
+            var orders = new List<Order>
+            {
+                new Order { Id = 101, ProductName = "Laptop", Price = 1500.00M, Quantity = 1 },
+                new Order { Id = 102, ProductName = "Mouse", Price = 20.00M, Quantity = 2 },
+                new Order { Id = 103, ProductName = "Keyboard", Price = 50.00M, Quantity = 1 }
+            };
+            var viewModel = new CustomerOrderViewModel
+            {
+                Customer = customer,
+                Orders = orders
+            };
+            return View(viewModel);
+
+        }
+    }
+}
